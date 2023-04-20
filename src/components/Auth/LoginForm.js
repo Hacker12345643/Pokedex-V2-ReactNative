bb066e81318f752f2fbde0bc75357ec6bc1d1755
Yoshida22 Dev <yoshio_salvador@hotmail.com>
2023-04-20 15:39:12 -0600

    Se agregaron las screen de LoginForms y UserData además de un usuario ficticio

---
+++ src/components/Auth/LoginForm.js
@@ -0,0 +1,17 @@
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  Keyboard,
+} from "react-native";
+import React from "react";
+
+export default function LoginForm() {
+  return (
+    <View>
+      <Text>LoginForm</Text>
+    </View>
+  );
+}
---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+  username: "Yoshito52",
+  password: "SaskecitoBB",
+};
+
+export const userDetails = {
+  username: "Yoshito52",
+  firstName: "Yoshio",
+  lastName: "Garcia",
+  email: "yostops@jkanime.com",
+};
