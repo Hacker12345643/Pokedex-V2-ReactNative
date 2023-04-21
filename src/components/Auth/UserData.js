105bc24c7f45a564b1704637f63ef4f2e17f884a
Yoshida22 Dev <yoshio_salvador@hotmail.com>
2023-04-20 21:34:53 -0600

    Se agregó información a cuenta y también un botón para desloguear

--- src/components/Auth/UserData.js
+++ src/components/Auth/UserData.js
@@ -1,10 +1,65 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { StyleSheet, View, Text, Button } from "react-native";
+import React from "react";
+import useAuth from "../../hooks/useAuth";
 
 export default function UserData() {
+  const { auth, logout } = useAuth();
+
   return (
-    <View>
-      <Text>UserData</Text>
+    <View style={styles.content}>
+      <View style={styles.titleBlock}>
+        <Text style={styles.title}>Bienvenido,</Text>
+        <Text style={styles.title}>{`${auth.firstName} ${auth.lastName}`}</Text>
+      </View>
+      <View style={styles.dataContent}>
+        <ItenMenu title="Nombre:" text={`${auth.firstName} ${auth.lastName}`} />
+        <ItenMenu title="Username:" text={auth.username} />
+        <ItenMenu title="Email:" text={auth.email} />
+        <ItenMenu title="Favoritos:" text={`0 pokemon`} />
+      </View>
+      <Button   title="Desconectarse" onPress={logout}/>
     </View>
-  )
+  );
 }
+
+function ItenMenu(props) {
+  const { title, text } = props;
+
+  return (
+    <View style={styles.itemMenu}>
+      <Text style={styles.itemMenuTitle}>{title}</Text>
+      <Text>{text}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  content: {
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  titleBlock: {
+    marginBottom: 30,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 22,
+  },
+  dataContent: {
+    marginBottom: 20,
+  },
+  itemMenu: {
+    flexDirection: "row",
+    paddingVertical: 20,
+    borderBottomWidth: 1,
+    borderColor: "#CFCFCF",
+  },
+  itemMenuTitle: {
+    fontWeight: "bold",
+    paddingRight: 10,
+    width: 120,
+  },
+  btnLogout: {
+    paddingTop: 100,
+  },
+});
